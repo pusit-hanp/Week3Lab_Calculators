747d1dae2dfc29e0f6022ff24e99ae2559a88145
pusit <pusit@PR-TUF>
2023-01-29 18:32:55 -0700

    Age Calculator working Version 1.0

---
+++ src/java/servlet/AgeCalculatorServlet.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pusit
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+    
+   @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+       getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+               .forward(request, response);
+   }
+   
+   @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+       String age = request.getParameter("age");
+       
+       request.setAttribute("age", age);
+       
+       if(age==null||age.equals("")){
+           request.setAttribute("message","You must give your current age");
+       } else {
+           try {
+               int nextAge = Integer.parseInt(age)+1;
+               request.setAttribute("message","Your age next birthday will be " + nextAge);
+           }
+           catch(NumberFormatException e){
+               request.setAttribute("message","You must enter a number.");
+           }
+       }
+       
+       getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+               .forward(request, response);
+   }
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : agecalculator
+    Created on : Jan 29, 2023, 5:49:36 PM
+    Author     : pusit
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form method="post" action="age">
+            Enter your age:<input type="text" name="age"><br>
+            <input type="submit" value="Age next birthday">
+        </form>
+        <p>${message}</p>
+        
+    </body>
+</html>
